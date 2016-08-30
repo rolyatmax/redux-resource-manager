1056e9b11e94281619861e33ef9f880fa0e7caf4
David Dunlop <david.dunlop@buzzfeed.com>
2016-08-30 17:36:35 -0400

    Throw status code on error

--- src/utils/fetch_json.js
+++ src/utils/fetch_json.js
@@ -1,3 +1,10 @@
+class FetchError extends Error {
+  constructor(msg, statusCode) {
+    super(msg);
+    this.statusCode = statusCode;
+  }
+}
+
 export default function fetchJSON(url, options = {}) {
   const requestOptions = {
     credentials: 'same-origin',
@@ -6,7 +13,7 @@ export default function fetchJSON(url, options = {}) {
 
   return fetch(url, requestOptions).then(response => {
     if (response.status < 200 || response.status >= 400) {
-      throw new Error(`${response.status} response from ${url}`);
+      throw new FetchError(`${response.status} response from ${url}`, response.status);
     }
     return response.json();
   });
